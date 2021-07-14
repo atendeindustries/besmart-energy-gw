72572dc17122e6b93c6f57d7c2df74df7c3c5fb0
Łukasz Olender <lukasz.olender@atende.industries>
2021-07-14 18:23:28 +0200

    Use define instead of const char arrays

--- gw/config.h
+++ gw/config.h
@@ -1,11 +1,16 @@
+#ifndef GATEWAY_CONFIG_H
+#define GATEWAY_CONFIG_H
+
 /* config */
-const uint16_t PORT = 443;
-static char TIME_API[] = "213.188.196.246";
-const char ENV_LOCAL[] = "127.0.0.1";
-const char ENV_DEV[] = "127.0.0.1";
-const char TOKEN[] = "";
-const char METER_TYPE_NAME[] = "";
-const char ENV_DEV_NAME[] = "";
-const char FIND_STATE_ENDPOINT[] = "";
-const char PUT_SIGNALS_DATA_ENDPOINT[] = "";
+#define PORT 443
+#define TIME_API "213.188.196.246"
+#define ENV_LOCAL "127.0.0.1"
+#define ENV_DEV "127.0.0.1"
+#define TOKEN ""
+#define METER_TYPE_NAME ""
+#define ENV_DEV_NAME "localhost"
+#define FIND_STATE_ENDPOINT ""
+#define PUT_SIGNALS_DATA_ENDPOINT ""
 // ------
+
+#endif
