8a9312547c0be1639d7027f20980141b8bfe9e78
Łukasz Olender <lukasz.olender@atende.industries>
2021-08-06 09:15:07 +0200

    Rewrite https client. Update request structure and refactor gateway code.

--- gw/gateway.c
+++ gw/gateway.c
@@ -13,55 +13,72 @@
 #include "../../metersrv/metersrv/sensor.h"
 #include "https.h"
 #include "config.h"
-
-void error(const char *msg) { 
-    printf("%s\n", msg);
-    exit(0);
+#include <sys/socket.h>
+#include <sys/select.h>
+void test() {
+    socket(0,0,0);
+    select(0, 0, 0, 0, 0);
 }
 
-static char ip[15];
+
 static char endpoint[128];
-static char data[1024];
-static char response[512];
+static char data[1280];
+static char response[1280];
 static char SERIAL[18];
 static int PHASES;
 static char METER_TYPE_NAME[6];
+static unsigned long long stateSince;
 
 struct sensorId {
     int client_cid;
     int sensor_mid;
 };
 
-void open_connection(HTTP_INFO* hi, char* ip) {
-    while (http_open(hi, ip) < 0)
-    {
+time_t getTime(HTTP_INFO* hi) {
+    int ret = -1;
+    http_set_host(hi, TIME_API_NAME, "80", 0);
+    http_set_token(hi, "");
+    do {
+        ret = http_open(hi);
+        if (ret != 0) http_close(hi);
         sleep(5);
-    }
-}
+    } while (ret != 0);
 
-unsigned long long getTime(HTTP_INFO* hi) {
-    open_connection(hi, TIME_API);
-    http_get(hi, "http://worldtimeapi.org/api/timezone/Europe/Warsaw", response, sizeof(response));
+    do {
+        ret = http_get(hi, "/api/timezone/Europe/Warsaw", response, sizeof(response));
+        if (ret < 200 || ret >= 400) sleep(5);
+    } while (ret < 200 || ret >= 400);
     http_close(hi);
     char* timeptr = strstr(response, "unixtime") + 10;
-    unsigned long long time = strtoull(timeptr, NULL, 0);
+    time_t time = strtoul(timeptr, NULL, 0);
+    if (DEBUG) printf("Returned unixtimestamp: %llu.\n", time);
     return time;
 }
 
 struct sensorId identify(HTTP_INFO* hi) {
-    sprintf(endpoint, "https://%s:%d%s?meter_dev=%s&meter_type_name=%s", ENV_DEV_NAME, PORT, FIND_STATE_ENDPOINT, SERIAL, METER_TYPE_NAME);
-    printf("%s - ", endpoint);
+    int res;
+
+    sprintf(endpoint, "%s?meter_dev=%s&meter_type_name=%s", FIND_STATE_ENDPOINT, SERIAL, METER_TYPE_NAME);
+    if (DEBUG) printf("%s - ", endpoint);
     fflush(stdout);
-    strcpy(hi->request.authorization, TOKEN);
-    int return_code = http_get(hi, endpoint, response, sizeof(response));
-    printf("%d\n", return_code);
-    printf("response: %s\n", response);
+
+    res = http_open(hi);
+    while (res != 0) {
+        http_close(hi);
+        res = http_open(hi);
+        sleep(5);
+    }
+    res = http_get(hi, endpoint, response, sizeof(response));
+    http_close(hi);
+    if (DEBUG) printf("%d\nresponse: %s\n", res, response);
 
     struct sensorId sid;
     char* client_cid = strstr(response, "client_cid");
     sid.client_cid = client_cid != NULL ? atoi(client_cid + 13) : 0;
     char* sensor_mid = strstr(response, "sensor_mid");
     sid.sensor_mid = sensor_mid != NULL ? atoi(sensor_mid + 13) : 0;
+    char* since = strstr(response, "since");
+    stateSince = since != NULL ? strtoull(since + 8, NULL, 0) : 0;
     
     return sid;
 }
@@ -69,83 +86,100 @@ struct sensorId identify(HTTP_INFO* hi) {
 int sendMeasurement(HTTP_INFO* hi, int cid, int mid, int moid, time_t timestamp, float value) {
     int return_code;
     sprintf(data,
-            "[{\n"
-            "    \"client_cid\": %d,\n"
-            "    \"sensor_mid\": %d,\n"
-            "    \"signal_type_moid\": %d,\n"
-            "    \"data\": [{\n"
-            "        \"time\": %llu,\n"
-            "        \"value\": %f,\n"
-            "        \"type\": \"DBL\",\n"
-            "        \"origin\": 1\n"
-            "    }]\n"
-            "}]\r\n",
+            "[{"
+            "\"client_cid\":%d,"
+            "\"sensor_mid\":%d,"
+            "\"signal_type_moid\":%d,"
+            "\"data\":{"
+            "\"time\":[%llu],"
+            "\"value\":[%f],"
+            "\"type\":[\"DBL\"],"
+            "\"origin\":[1]"
+            "}"
+            "}]",
             cid,
             mid,
             moid,
             timestamp,
             value
     );
-    printf("req: %s\n\n", data);
+    if (DEBUG) printf("req: %s\n\n", data);
 
-    strcpy(hi->request.authorization, TOKEN);
-    sprintf(endpoint, "https://%s:%d%s", ENV_DEV_NAME, PORT, PUT_SIGNALS_DATA_ENDPOINT);
-    return_code = http_put_post("PUT", hi, endpoint, data, response, sizeof(response));
+    do {
+        return_code = http_put(hi, PUT_SIGNALS_DATA_ENDPOINT, data, response, sizeof(response));
+        if (return_code < 0) sleep(2);
+    } while (return_code < 0);
 
-    printf("Status: %d\n", return_code);
-    printf("return body: %s \n", response);
+    if (DEBUG) printf("Status: %d\n", return_code);
+    if (DEBUG) printf("return body: %s \n", response);
 
     return return_code;
 }
 
 unsigned long long getLastCap(HTTP_INFO* hi, int cid, int mid) {
-    strcpy(hi->request.authorization, TOKEN);
-    sprintf(endpoint, "https://%s:%d%s/%d.%d/cap", ENV_DEV_NAME, PORT, SENSORS_API, cid, mid);
-    http_get(hi, endpoint, response, sizeof(response));
+    int ret;
+    sprintf(endpoint, "%s/%d.%d/signals/cap?signal_type_moid=32&signal_origin_id=1", SENSORS_API, cid, mid);
+    ret = http_get(hi, endpoint, response, sizeof(response));
+    while(ret < 0) {
+        sleep(1);
+        ret = http_get(hi, endpoint, response, sizeof(response));
+    };
     
     char* p1 = strchr(response, '{');
     char* p2 = strchr(response, '}');
-    unsigned long long last_timestamp = 0;
-    while (last_timestamp == 0 && p1 != NULL) {
+    unsigned long long lastTimestamp = 0;
+    while (lastTimestamp == 0 && p1 != NULL) {
         *p2 = 0;
         if (strstr(response, "signal_origin_id\": 1") != NULL &&
             strstr(response, "signal_type_moid\": 32") != NULL) {
             
             p1 = strstr(response, "cap_max\":") + 10;
-            last_timestamp = strtoull(p1, NULL, 0);
+            lastTimestamp = strtoull(p1, NULL, 0);
+            break;
         } else {
             p1 = p2 + 1;
             p2 = strchr(p1, '}');
             if (p2 == NULL) p1 = NULL;
         }
     }
-    return last_timestamp / 1000;
+    lastTimestamp = lastTimestamp > 0 ? lastTimestamp / 1000 : stateSince / 1000;
+    if (DEBUG) printf("Last cap: %llu\n", lastTimestamp);
+    return lastTimestamp;
 }
 
-void fillGaps(HTTP_INFO* hi, oid_t* oid, int cid, int mid, unsigned long long current) {
+void sendProfileData(HTTP_INFO* hi, oid_t* oid, int cid, int mid, unsigned long long current) {
     MeterBasicResult_t* result;
     time_t timestamp;
     unsigned long long lastCap = getLastCap(hi, cid, mid);
+    unsigned long long tmp = lastCap;
     int res;
     msg_t msg;
     current -= current % 900;
-    lastCap += 900;
-    while (lastCap <= current) {
-        unsigned int diff = (int)(current - lastCap) / 900;
+    tmp -= tmp % 900;
+    while (tmp <= current) {
+        unsigned int diff = (int)(current - tmp) / 900;
         msg.type = 1;
         memcpy(msg.o.raw, &diff, sizeof(unsigned int));
         if ((res = msgSend(oid->port, &msg)) < 0 || msg.o.io.err == -1) {
-            return;
+            if (DEBUG) printf("Could not fill gap (-d %u)\n", diff);
+            tmp += 900;
+            continue;
         }
         result = *(MeterBasicResult_t **)msg.o.raw;
         timestamp = *(time_t *)(msg.o.raw +
             sizeof(MeterBasicResult_t *));
+
+        if (timestamp <= lastCap) {
+            if (DEBUG) printf("Timestamp in profile lower or equal to lastCap (-d %u)\n", diff);
+            tmp += 900;
+            continue;
+        }
         sendMeasurement(hi, cid, mid, 32, timestamp * 1000, ((float)result->eactive_plus_sum.value) / 1000.0 / 3600.0);
         sendMeasurement(hi, cid, mid, 34, timestamp * 1000, ((float)result->eactive_minus_sum.value) / 1000.0 / 3600.0);
         sendMeasurement(hi, cid, mid, 44, timestamp * 1000, ((float)result->eapparent_plus_sum.value) / 1000.0 / 3600.0);
         sendMeasurement(hi, cid, mid, 46, timestamp * 1000, ((float)result->eapparent_minus_sum.value) / 1000.0 / 3600.0);
-
-        lastCap += 900;
+        lastCap = timestamp;
+        tmp += 900;
     }
 }
 
@@ -153,159 +187,119 @@ int main(int argc, char **argv)
 {
     srand(time(NULL));
 
-    if (argc < 2) {
-        error("Usage: /bin/sockettest [env]");
-    }
-    
-    /* Setting host */
-
-    char* env = argv[1];
-
-    if (strcmp("local", env) == 0) {
-        strcpy(ip, ENV_LOCAL);
-    } else if (strcmp("dev", env) == 0) {
-        strcpy(ip, ENV_DEV);
-    } else {
-        error("Invalid env. Available: local/dev");
-    }
-
     msg_t msg;
     int res;
-    unsigned int arg = 0;
     oid_t oid;
     MeterBasicResult_t *result;
     meter_state_t state;
     time_t timestamp;
+    struct sensorId sid;
+    sid.client_cid = 0;
+    sid.sensor_mid = 0;
     int result_code;
 
     while (lookup("/dev/metersrv", NULL, &oid) < 0) {
-		sleep(1);
-	}
-
-    msg.type = 5;
-    if ((res = msgSend(oid.port, &msg)) < 0 || msg.o.io.err == -1) {
-        error("Could not get meter status");
-    }
-    result = *(MeterBasicResult_t **)msg.o.raw;
-    state = *(meter_state_t *)(msg.o.raw + sizeof(MeterBasicResult_t *) + sizeof(MeterConf_t *));
-    strcpy(SERIAL, state.serial);
-
-    if (SERIAL[5] == '3') {
-        PHASES = 3;
-        strcpy(METER_TYPE_NAME, "EM3Ph");
-    } else {
-        PHASES = 1;
-        strcpy(METER_TYPE_NAME, "EM1Ph");
+        sleep(5);
     }
 
-    HTTP_INFO hi1;
-    http_init(&hi1, FALSE);
-    unsigned long long currentTimestamp = getTime(&hi1);
-
-    msg.type = 7;
-    memcpy(msg.o.raw, &currentTimestamp, sizeof(currentTimestamp));
-    if ((res = msgSend(oid.port, &msg)) < 0 || msg.o.io.err == -1) {
-        printf("Could not set meter time (%d)", res);
+    HTTP_INFO hi;
+    timestamp = getTime(&hi);
+    if (timestamp > 1e9) {
+        msg.type = 7;
+        memcpy(msg.o.raw, &timestamp, sizeof(timestamp));
+        if ((res = msgSend(oid.port, &msg)) < 0 || msg.o.io.err == -1) {
+            printf("Could not set meter time (%d)", res);
+        }
     }
 
-    open_connection(&hi1, ip);
-    struct sensorId sid = identify(&hi1);
+    http_setup(&hi);
+    http_set_host(&hi, API_NAME, PORT, 1);
+    http_set_token(&hi, TOKEN);
 
-    if (sid.client_cid > 0 && sid.sensor_mid > 0) {
-        printf("Identified (cid: %d, mid: %d)\n\n", sid.client_cid, sid.sensor_mid);
-    } else {
-        error("Couldn't identify meter.");
+    do {
+        msg.type = 5;
+        if ((res = msgSend(oid.port, &msg)) < 0 || msg.o.io.err == -1) {
+            printf("Could not get status from metersrv (%d)", res);
+            sleep(5);
+            continue;
+        }
+        result = *(MeterBasicResult_t **)msg.o.raw;
+        timestamp = *(time_t *)(msg.o.raw + sizeof(MeterBasicResult_t *) +
+            sizeof(MeterConf_t *) + sizeof(meter_state_t));
+        state = *(meter_state_t *)(msg.o.raw + sizeof(MeterBasicResult_t *) + sizeof(MeterConf_t *));
+        strcpy(SERIAL, state.serial);
+        if (SERIAL[5] == '3') {
+            PHASES = 3;
+            strcpy(METER_TYPE_NAME, "EM3Ph");
+        } else {
+            PHASES = 1;
+            strcpy(METER_TYPE_NAME, "EM1Ph");
+        }
+
+        sid = identify(&hi);
+        if (sid.client_cid == 0 && sid.sensor_mid == 0) {
+            printf("Couldn't identify meter in besmart.energy. Waiting 1m...\n");
+            sleep(60);
+        }
+    } while (sid.client_cid == 0 && sid.sensor_mid == 0);
+
+    if (DEBUG) printf("Identified (cid: %d, mid: %d)\n\n", sid.client_cid, sid.sensor_mid);
+
+
+    res = http_open(&hi);
+    while (res != 0) {
+        http_close(&hi);
+        res = http_open(&hi);
+        sleep(5);
     }
-
-    fillGaps(&hi1, &oid, sid.client_cid, sid.sensor_mid, currentTimestamp);
+    sendProfileData(&hi, &oid, sid.client_cid, sid.sensor_mid, timestamp);
+    http_close(&hi);
 
     while (1) {
         msg.type = 5;
         if ((res = msgSend(oid.port, &msg)) < 0 || msg.o.io.err == -1) {
             printf("Could not get status from metersrv (%d)", res);
-            sleep(1);
+            sleep(60);
             continue;
         }
-
         result = *(MeterBasicResult_t **)msg.o.raw;
         timestamp = *(time_t *)(msg.o.raw + sizeof(MeterBasicResult_t *) +
             sizeof(MeterConf_t *) + sizeof(meter_state_t));
+
+        res = http_open(&hi);
+        if (res != 0) {
+            http_close(&hi);
+            sleep(60);
+            continue;
+        }
         // V1
-        result_code = sendMeasurement(&hi1, sid.client_cid, sid.sensor_mid, 53, timestamp * 1000, result->u_rms_avg[0]);
+        result_code = sendMeasurement(&hi, sid.client_cid, sid.sensor_mid, 53, timestamp * 1000, result->u_rms_avg[0]);
         // I1
         if (result_code > -1)
-            result_code = sendMeasurement(&hi1, sid.client_cid, sid.sensor_mid, 56, timestamp * 1000, result->i_rms_avg[0]);
+            result_code = sendMeasurement(&hi, sid.client_cid, sid.sensor_mid, 56, timestamp * 1000, result->i_rms_avg[0]);
 
         if (PHASES == 3) {
             // V2
             if (result_code > -1)
-                result_code = sendMeasurement(&hi1, sid.client_cid, sid.sensor_mid, 54, timestamp * 1000, result->u_rms_avg[0]);
+                result_code = sendMeasurement(&hi, sid.client_cid, sid.sensor_mid, 54, timestamp * 1000, result->u_rms_avg[1]);
             // I2
             if (result_code > -1)
-                result_code = sendMeasurement(&hi1, sid.client_cid, sid.sensor_mid, 57, timestamp * 1000, result->i_rms_avg[0]);
+                result_code = sendMeasurement(&hi, sid.client_cid, sid.sensor_mid, 57, timestamp * 1000, result->i_rms_avg[1]);
             // V3
             if (result_code > -1)
-                result_code = sendMeasurement(&hi1, sid.client_cid, sid.sensor_mid, 55, timestamp * 1000, result->u_rms_avg[0]);
+                result_code = sendMeasurement(&hi, sid.client_cid, sid.sensor_mid, 55, timestamp * 1000, result->u_rms_avg[2]);
             // I3
             if (result_code > -1)
-                result_code = sendMeasurement(&hi1, sid.client_cid, sid.sensor_mid, 58, timestamp * 1000, result->i_rms_avg[0]);
+                result_code = sendMeasurement(&hi, sid.client_cid, sid.sensor_mid, 58, timestamp * 1000, result->i_rms_avg[2]);
         }
-
         if (result_code > -1 && (timestamp / 60) % 15 == 1) {
-            msg.type = 1;
-            memcpy(msg.o.raw, &arg, sizeof(unsigned int));
-            if ((res = msgSend(oid.port, &msg)) < 0 || msg.o.io.err == -1) {
-                printf("Could not get info from metersrv (%d)", res);
-                sleep(1);
-                continue;
-            }
-            result = *(MeterBasicResult_t **)msg.o.raw;
-            timestamp = *(time_t *)(msg.o.raw +
-                sizeof(MeterBasicResult_t *));
-            // A+
-            result_code = sendMeasurement(
-                &hi1, sid.client_cid, sid.sensor_mid, 32, timestamp * 1000,
-                ((float)result->eactive_plus_sum.value) / 1000.0 / 3600.0
-            );
-            // A-
-            if (result_code > -1)
-                result_code = sendMeasurement(
-                    &hi1, sid.client_cid, sid.sensor_mid, 34, timestamp * 1000,
-                    ((float)result->eactive_minus_sum.value) / 1000.0 / 3600.0
-                );
-            // S+
-            if (result_code > -1)
-                result_code = sendMeasurement(
-                    &hi1, sid.client_cid, sid.sensor_mid, 44, timestamp * 1000,
-                    ((float)result->eapparent_plus_sum.value) / 1000.0 / 3600.0
-                );
-            // S-
-            if (result_code > -1)
-                result_code = sendMeasurement(
-                    &hi1, sid.client_cid, sid.sensor_mid, 46, timestamp * 1000,
-                    ((float)result->eapparent_minus_sum.value) / 1000.0 / 3600.0
-                );
+            sendProfileData(&hi, &oid, sid.client_cid, sid.sensor_mid, timestamp);
         }
-        if (result_code == -1) {
-            printf("Disconnected...\n");
-            http_close(&hi1);
-            open_connection(&hi1, ip);
-            msg.type = 5;
-            if ((res = msgSend(oid.port, &msg)) < 0 || msg.o.io.err == -1) {
-                printf("Could not get status from metersrv (%d)", res);
-                sleep(1);
-                continue;
-            }
+        http_close(&hi);
 
-            result = *(MeterBasicResult_t **)msg.o.raw;
-            currentTimestamp = *(time_t *)(msg.o.raw + sizeof(MeterBasicResult_t *) +
-                sizeof(MeterConf_t *) + sizeof(meter_state_t));
-            fillGaps(&hi1, &oid, sid.client_cid, sid.sensor_mid, currentTimestamp);
-        }
-
-        printf("\n\nSleeping 1m...\n\n");
+        if (DEBUG) printf("\n\nSleeping 1m...\n\n");
         sleep(60);
     }
-
-	return 0;
+    http_destroy(&hi);
+    return 0;
 }
--- gw/https.h
+++ gw/https.h
@@ -1,102 +1,49 @@
-//
-// Created by HISONA on 2016. 2. 29..
-//
-
 #ifndef HTTPS_CLIENT_HTTPS_H
 #define HTTPS_CLIENT_HTTPS_H
 
-/*---------------------------------------------------------------------*/
-#include "mbedtls/include/mbedtls/net.h"
+#include "mbedtls/include/mbedtls/net_sockets.h"
 #include "mbedtls/include/mbedtls/entropy.h"
 #include "mbedtls/include/mbedtls/ctr_drbg.h"
 #include "mbedtls/include/mbedtls/error.h"
-#include "mbedtls/include/mbedtls/certs.h"
 
-/*---------------------------------------------------------------------*/
-#define H_FIELD_SIZE     128
-#define H_READ_SIZE     512
-
-#undef TRUE
-#undef FALSE
-
-#define TRUE    1
-#define FALSE   0
-
-typedef unsigned char BOOL;
-
-typedef struct
-{
-    char method[8];
-    int  status;
-    char content_type[H_FIELD_SIZE];
-    long content_length;
-    BOOL chunked;
-    BOOL close;
-    char location[H_FIELD_SIZE];
-    char referrer[H_FIELD_SIZE];
-    char cookie[H_FIELD_SIZE];
-    char boundary[H_FIELD_SIZE];
-    char authorization[H_FIELD_SIZE];
-
-} HTTP_HEADER;
-
-typedef struct
-{
-    BOOL    verify;
-
-    mbedtls_net_context         ssl_fd;
+typedef char bool;
+typedef struct {
+    mbedtls_net_context         server_fd;
     mbedtls_entropy_context     entropy;
     mbedtls_ctr_drbg_context    ctr_drbg;
     mbedtls_ssl_context         ssl;
     mbedtls_ssl_config          conf;
     mbedtls_x509_crt            cacert;
-
 } HTTP_SSL;
 
 typedef struct {
+    char* userAgent;
+    char* contentType;
+    char* bearerToken;
+} HTTP_REQUEST_HEADERS;
 
-    BOOL    https;
-    char    host[256];
-    char    port[8];
-    char    path[H_FIELD_SIZE];
-
-} HTTP_URL;
-
-typedef struct
-{
-    HTTP_URL    url;
-
-    HTTP_HEADER request;
-    HTTP_HEADER response;
-    HTTP_SSL    tls;
-
-    long        length;
-    char        r_buf[H_READ_SIZE];
-    long        r_len;
-    BOOL        header_end;
-    char        *body;
-    long        body_size;
-    long        body_len;
-
-
+typedef struct {
+    int status;
+    int contentLength;
+} HTTP_RESPONSE_HEADERS;
+typedef struct {
+    HTTP_REQUEST_HEADERS requestHeaders;
+    HTTP_RESPONSE_HEADERS responseHeaders;
+    HTTP_SSL httpSSL;
+    char* addr;
+    char* host;
+    char* port;
+    bool ssl;
 } HTTP_INFO;
 
-
-/*---------------------------------------------------------------------*/
-
-char *strtoken(char *src, char *dst, int size);
-
-int  http_init(HTTP_INFO *hi, BOOL verify);
-int  http_close(HTTP_INFO *hi);
-int  http_get(HTTP_INFO *hi, char *url, char *response, int size);
-int  http_put_post(char* method, HTTP_INFO *hi, char *url, char *data, char *response, int size);
-
-void http_strerror(char *buf, int len);
-int  http_open(HTTP_INFO *hi, char *url);
-int  http_write_header(HTTP_INFO *hi);
-int  http_write(HTTP_INFO *hi, char *data, int len);
-int  http_write_end(HTTP_INFO *hi);
-int  http_read_chunked(HTTP_INFO *hi, char *response, int size);
+void http_set_host(HTTP_INFO* hi, char* host, char* port, bool ssl);
+void http_set_token(HTTP_INFO* hi, char* token);
+int http_get(HTTP_INFO* hi, char* endpoint, char* buffer, int size);
+int http_put(HTTP_INFO* hi, char* endpoint, char* data, char* buffer, int size);
+int http_setup(HTTP_INFO* hi);
+void http_destroy(HTTP_INFO* hi);
+int http_open(HTTP_INFO* hi);
+int http_close(HTTP_INFO* hi);
 
 #endif //HTTPS_CLIENT_HTTPS_H
 
